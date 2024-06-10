ce7e95c630fdae1fb1ddbf87190758db148935d9
patelshehbaz <patelshehbaz27@gmail.com>
2024-06-09 21:16:03 -0400

    package created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="IQC",
+    version="0.0.0",
+    author="Shehbaz Patel",
+    author_email="patelshehbaz27@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
